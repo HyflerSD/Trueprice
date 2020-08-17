4b76e59995fd63519096f7a77121279f09f6c24e
hyflersd <hyflersd@gmail.com>
2020-08-17 18:02:33 -0400

    gunicorn fix

--- TruePrice/settings.py
+++ TruePrice/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a4mkrpst5l27(te6%q@t-tfvacy6p+(6a#ora2hv*o6q5db!*w'
+SECRET_KEY = SECRETKEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
